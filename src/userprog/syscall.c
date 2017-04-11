c96ab7cdd8ecd73bb6ae80bd27b097ec90356f73
lenabanks <lena.banks@ucdenver.edu>
2017-04-11 00:31:30 -0600

    added parsing for sys_call number from user_stack running args-many test:   executes,   parses,   tries to call sys_call: write and     fails due to write not being fully implemented yet     (this is expected per Pintos manual: 3.2)

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -13,14 +14,91 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) {
+syscall_handler (struct intr_frame *f) {
   printf("system call!\n");
 
   //TODO check all the pointers n such: NOT NULL..., anything else?
   //TODO need to check pointer to each arg as well?
-  if (!is_user_vaddr(f.esp)){
+  if (!is_user_vaddr(f->esp)){
+    printf("Don't gimme that crap");
     thread_exit();
   }
+
+  uint8_t * sys_call_num = get_user(f->esp);
+  
+  switch(sys_call_num){
+    case SYS_HALT :
+      printf("calling SYS_HALT");
+      break;                   /* Halt the operating system. */
+    case SYS_EXIT :
+      printf("calling SYS_EXIT");
+      break;                   /* Terminate this process. */
+    case SYS_EXEC :
+      printf("calling SYS_EXEC");
+      break;                   /* Start another process. */
+    case SYS_WAIT :
+      printf("calling SYS_WAIT");
+      break;                   /* Wait for a child process to die. */
+    case SYS_CREATE :
+      printf("calling SYS_CREATE");
+      break;                 /* Create a file. */
+    case SYS_REMOVE :
+      printf("calling SYS_REMOVE");
+      break;                 /* Delete a file. */
+    case SYS_OPEN :
+      printf("calling SYS_OPEN");
+      break;                   /* Open a file. */
+    case SYS_FILESIZE :
+      printf("calling SYS_FILESIZE");
+      break;               /* Obtain a file's size. */
+    case SYS_READ :
+      printf("calling SYS_READ");
+      break;                   /* Read from a file. */
+    case SYS_WRITE :
+      printf("calling SYS_WRITE");
+      break;                  /* Write to a file. */
+    case SYS_SEEK :
+      printf("calling SYS_SEEK");
+      break;                   /* Change position in a file. */
+    case SYS_TELL :
+      printf("calling SYS_TELL");
+      break;                   /* Report current position in a file. */
+    case SYS_CLOSE :
+      printf("calling SYS_CLOSE");
+      break;                  /* Close a file. */
+
+      /* Project 3 and optionally project 4. */
+    case SYS_MMAP :
+      printf("calling SYS_MMAP");
+      break;                   /* Map a file into memory. */
+    case SYS_MUNMAP :
+      printf("calling SYS_MUNMAP");
+      break;                 /* Remove a memory mapping. */
+
+      /* Project 4 only. */
+    case SYS_CHDIR :
+      printf("calling SYS_CHDIR");
+      break;                  /* Change the current directory. */
+    case SYS_MKDIR :
+      printf("calling SYS_MKDIR");
+      break;                  /* Create a directory. */
+    case SYS_READDIR :
+      printf("calling SYS_READDIR");
+      break;                /* Reads a directory entry. */
+    case SYS_ISDIR :
+      printf("calling SYS_ISDIR");
+      break;                  /* Tests if a fd represents a directory. */
+    case SYS_INUMBER:
+      printf("calling SYS_INUMBER");
+      break;
+    default:
+      break;
+      
+    
+  }
+
+
+
   //TODO process which system call is being referenced in syscall-nr.h then call corresponding function here, passing *f (parse *f here? probly not, coupling n such
 
 
