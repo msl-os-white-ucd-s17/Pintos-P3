c2f42239b01ea7ff610be187ce1aa7b73aecb6bb
ShawnAlexander <shawn.a.johnson@ucdenver.edu>
2017-05-08 13:11:00 -0600

    Swap almost done

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -29,21 +29,28 @@ void init_swap() {
 }
 
 void swap_in(struct page *pin) {
-    swap_block = block_get_role(BLOCK_SWAP);        // Get block with SWAP role
     ASSERT (pin != NULL);
+    ASSERT (pin->frame != NULL);
+    ASSERT (lock_held_by_current_thread(&pin->frame->lock));
     lock_acquire(&swap_lock);
-    
-    
-    
+    ASSERT (bitmap_test(SWAP_TABLE, (size_t) pin->sector) == 1);
+    for (unsigned int i = 0; i < PAGE_SECTORS; ++i) {
+        block_sector_t sector_idx = pin->sector + i;
+        void *sec_loc = pin->frame->base + i * BLOCK_SECTOR_SIZE;
+        block_read(SWAP, sector_idx, sec_loc);
+    }
+    bitmap_set(SWAP_TABLE, (size_t) pin->sector, 0);
+    pin->sector = -1;
     lock_release(&swap_lock);
 }
 
 /* Probably should make a page/frame evict function that calls this one.*/
+/* Outside of this call we must disassociate the frame from the swapped out page */
 bool swap_out(struct page *pout) {
     ASSERT (pout != NULL);
-
+    ASSERT (lock_held_by_current_thread(&pout->frame->lock));
     lock_acquire(&swap_lock);
-    /* Returns index of the beginning of a continuous region that is at least 8 sectors long */
+    /* Returns index of the first available page sector */
     osec = bitmap_scan_and_flip(SWAP_TABLE, 0, 1, 1);
     lock_release(&swap_lock);
     if (osec == BITMAP_ERROR) {
@@ -51,11 +58,13 @@ bool swap_out(struct page *pout) {
     }
 
     pout->sector = (block_sector_t) osec;
+    pout->private = true;
+    pout->file = NULL;
 
     /* Actual writing of frame to disk. */
     for (unsigned int i = 0; i < PAGE_SECTORS; ++i) {
         block_sector_t sector_idx = pout->sector + i;
-        void *frame_loc = p->frame->base + BLOCK_SECTOR_SIZE * i;
+        void *frame_loc = pout->frame->base + BLOCK_SECTOR_SIZE * i;
         block_write(SWAP, sector_idx, frame_loc);
     }
     return true;
